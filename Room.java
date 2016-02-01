54b74d6dfd732625e0316dd762f22efe586e48b0
superspindel <viktor.fallman@hotmail.com>
2016-02-01 16:09:40 +0100

    Assigment 2 room.java

--- Room.java
+++ Room.java
@@ -3,23 +3,30 @@ package lab2.level;
 
 import java.awt.Color;
 
+import lab2.Driver;
 
-public class Room { 
+
+public class Room {
+	int dx, dy;
+	Color color;
+	Room north, west, east, south = null;
 	
 	public Room(int dx, int dy, Color color) {
-		
+		this.dx = dx;
+		this.dy = dy;
+		this.color = color;
 	}
 
 	public void connectNorthTo(Room r) {
-		
+		this.north = r;
 	}
 	public void connectEastTo(Room r) {
-		
+		this.east = r;
 	}
 	public void connectSouthTo(Room r) {
-		
+		this.south = r;
 	}
 	public void connectWestTo(Room r) {
-		
+		this.west = r;
 	}
 }
