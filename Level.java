da8ac666f7fd454dff343c673fef2d64ef1e9e03
alcr33k <alex.alle@hotmail.com>
2016-02-03 18:52:19 +0100

    Update Level.java

--- Level.java
+++ Level.java
@@ -3,33 +3,32 @@ package lab2.level;
 
 import java.util.Observable;
 
+import lab2.level.Room;
 
 public class Level extends Observable {
-	public static int[] minX;
-    	public static int[] maxX;
-    	public static int[] minY;
-    	public static int[] maxY;
-   
+	public Room[] rooms;
+	// Room[] array = new Room[100];
+	Room location = null;
   	public boolean place(Room r, int x, int y)  {
-	for(int i = 0; i < minX.length; i++) { // check x-range
-          	if((x > minX[i]) && (x < maxX[i])) {
+	for(int i = 0; i < rooms.length; i++) { // check x-range
+          	if((x > rooms[i].px) && (x < rooms[i].px)) {
           	      return false;
             	}
         	}
-       		for(int i = 0; i < minY.length; i++) { // check y-range
-       	     		if((y > minY[i]) && (y < maxY[i])) {
+       		for(int i = 0; i < rooms.length; i++) { // check y-range
+       	     		if((y > rooms[i].py) && (y < rooms[i].py)) {
         	        	return false;
         	  	 }
         	} // all ok, add room
-        	minX[minX.length-1] = x;
-        	minY[minY.length-1] = y;
-        	maxY[minY.length-1] = y + Room.y;
-        	maxX[minX.length-1] = x + Room.x;   
+       		r.px = x;
+       		r.dx = y;
+        	rooms[rooms.length-1] = r;
         	return true;
     	}
 	
 	public void firstLocation(Room r) {
-			
+		// add here 
+		location = r;
 	}
 	
 }
